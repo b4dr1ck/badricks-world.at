078d4f3d9b3cbb5f85016c96946ff25701180f46
Patrick Reiter <patrick.reiter@itsv.at>
2025-10-20 09:19:11 +0200

    CHANGE: src/App.vue: / entfernt in IMG-Pfad

--- src/App.vue
+++ src/App.vue
@@ -35,8 +35,9 @@ export default {
       cover
       v-for="(img, index) in carouselImg"
       :key="'carousel-' + index"
-      :src="`${settings.imgOrigPath}/${img}`"
-      :lazy-src="`${settings.imgSmallPath}/${img}`"
+      :src="`${settings.imgOrigPath}${img}`"
+      :lazy-src="`${settings.imgSmallPath}${img}`"
+      :alt="img"
       cycle
     ></v-carousel-item>
   </v-carousel>
@@ -47,13 +48,14 @@ export default {
       v-for="(img, index) in imgData"
       :key="'gallery-' + index"
       :img="img"
+      :alt="img"
       @click="showImage($event)"
       :height="settings.imgThumbHeight"
       :width="settings.imgThumbWidth"
       class="ma-1"
       cover
-      :lazy-src="`${settings.imgSmallPath}/${img}`"
-      :src="`${settings.imgOrigPath}/${img}`"
+      :lazy-src="`${settings.imgSmallPath}${img}`"
+      :src="`${settings.imgOrigPath}${img}`"
     >
       <template v-slot:placeholder>
         <div class="d-flex align-center justify-center fill-height">
