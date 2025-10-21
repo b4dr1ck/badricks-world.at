db4e17ff9100ae3ccec5d4272d4c8bc94d707f0d
Patrick Reiter <patrick.reiter@itsv.at>
2025-10-21 12:43:24 +0200

    CHANGE: install.sh: Lege Sub-Dir's an wenn nicht existiert

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 destination_dir="$1"
-assets_dir="$2" 
-repo_dir="$3" 
+assets_dir="$2"
+repo_dir="$3"
 
 
 rtcCheck() {
@@ -47,7 +47,21 @@ if ls $assets_dir/*.png > /dev/null 2>&1; then
     echo "Convert..."
     convert -verbose -resize 5% "$img" "$assets_dir/small_$filename"
     echo ""
-
+    
+    if [ ! -d "$destination_dir/img/original" ]; then
+      echo "* Creating destination subdirectories..."
+      mkdir -vp "$destination_dir/img/original"
+      rtcCheck $?
+      echo ""
+    fi
+        
+    if [ ! -d "$destination_dir/img/small" ]; then
+      echo "* Creating destination subdirectories..."
+      mkdir -vp "$destination_dir/img/small"
+      rtcCheck $?
+      echo ""
+    fi
+    
     echo "Move..."
     mv -v "$img" "$destination_dir/img/original/"
     rtcCheck $?
@@ -58,20 +72,26 @@ if ls $assets_dir/*.png > /dev/null 2>&1; then
   done
 fi
 
-echo "* Building the project..."
-cd $repo_dir 
+echo "* Pull Repository"
+
+cd $repo_dir
+
+git pull origin master
+rtcCheck $?
+
+echo "* Install dependencies and build project"
 npm install
 rtcCheck $?
 npm run build
 rtcCheck $?
+echo ""
 
+echo "* Deploying build to '$destination_dir'..."
 cd dist || {
   echo "Error: 'dist' directory does not exist after build."
   exit 5
 }
 
-echo ""
-echo "* Deploying build to '$destination_dir'..."
 cp -rv ./* "$destination_dir/"
 rtcCheck $?
 
